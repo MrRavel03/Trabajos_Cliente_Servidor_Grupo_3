57ef56f0492915929185398af98008f1959326c6
Gabriel Osorio <gosorio3906@gmail.com>
2025-11-19 04:11:42 -0600

    Carpeta, libro, y base de datos

---
+++ Proyecto_Final_Grupo3/src/main/java/biblioteca/main/Main.java
@@ -0,0 +1,11 @@
+package biblioteca.main;
+
+import biblioteca.model.Libro;
+
+public class Main {
+    static void main() {
+
+        Libro i = new Libro(1,"Libro viejo","Angie Torres","Hola",true,"Hola");
+
+    }
+}
