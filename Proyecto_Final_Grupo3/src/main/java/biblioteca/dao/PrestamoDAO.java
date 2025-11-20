5f0ea6b689f0d45191ad0f8c1d7add815db8892c
Gabriel Osorio <gosorio3906@gmail.com>
2025-11-20 01:34:04 -0600

    completando prestamos

--- Proyecto_Final_Grupo3/src/main/java/biblioteca/dao/PrestamoDAO.java
+++ Proyecto_Final_Grupo3/src/main/java/biblioteca/dao/PrestamoDAO.java
@@ -3,12 +3,48 @@ package biblioteca.dao;
 import biblioteca.config.ConexionDB;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class PrestamoDAO {
 
-    public Boolean regristrarPrestamo(int idUsuario, int idLibro){
+    public boolean regristrarPrestamo(int idUsuario, int idLibro){
 
+        // conectamos con la dase de datos
         Connection con = ConexionDB.conectar();
 
+        PreparedStatement psInsert = null;
+        PreparedStatement psUpdate = null;
+
+        boolean resultado = false;
+
+        String sqlInsert = "INSERT INTO PRESTAMOS (ID_USUARIO, ID_LIBRO, " +
+                "FECHA_SALIDA, ID_ESTADO) VALUES " +
+                "(?, ?, CURRENT_DATE, 1)";
+
+        String sqlUpdateLibro = "UPDATE LIBROS SET DISPONIBLE = FALSE WHERE " +
+                "ID = ?";
+
+        try{
+            con.setAutoCommit(false);
+
+            //Insertamos el prestamo
+            psInsert = con.prepareStatement(sqlInsert);
+            psInsert.setInt(1,idUsuario);
+            psInsert.setInt(2, idLibro);
+            psInsert.executeUpdate();
+
+            //Marcamos 
+
+        } catch (SQLException e) {
+            System.err.println("Error en transaccion de préstamo:" + e.getMessage());
+            try{
+                if (con != null){
+                    con.rollback();
+                }
+            } catch (SQLException ex) {
+                System.err.println("Error: rollback falló: " + ex.getMessage());
+            }
+        }
     }
 }
--- Proyecto_Final_Grupo3/src/main/java/biblioteca/main/Main.java
+++ Proyecto_Final_Grupo3/src/main/java/biblioteca/main/Main.java
@@ -1,11 +1,15 @@
 package biblioteca.main;
 
+import biblioteca.config.ConexionDB;
 import biblioteca.model.Libro;
+
+import java.sql.Connection;
 
 public class Main {
     static void main() {
 
-        Libro i = new Libro(1,"Libro viejo","Angie Torres","Hola",true,"Hola");
+        //Libro i = new Libro(1,"Libro viejo","Angie Torres","Hola",true,"Hola");
 
+        //Connection con = ConexionDB.conectar();
     }
 }
