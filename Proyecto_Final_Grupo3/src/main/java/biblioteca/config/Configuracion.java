b4fd3b8d2151aa73cf234bcbde538132849c47eb
Gabriel Osorio <gosorio3906@gmail.com>
2025-11-22 11:05:12 -0600

    Creaaion Configuracion.java y Multa.java

---
+++ Proyecto_Final_Grupo3/src/main/java/biblioteca/config/Configuracion.java
@@ -0,0 +1,8 @@
+package biblioteca.config;
+
+public class Configuracion {
+
+    public static final double COSTO_MULTA_POR_DIA = 500.0;
+    public static final int DIAS_PRESTAMO_GRATIS = 7;
+
+}
---
+++ Proyecto_Final_Grupo3/src/main/java/biblioteca/model/Multa.java
@@ -0,0 +1,92 @@
+package biblioteca.model;
+
+import java.util.Date;
+
+public class Multa {
+
+    private int id;
+    private int idUsuario;
+    private String nombreUsuario;
+    private int idPrestamo;
+    private String tituloLibro;
+    private double monto;
+    private Date fechaGeneracion;
+    private String estado;
+
+    public Multa(){}
+
+    public Multa(int id, int idUsuario, String nombreUsuario, int idPrestamo, String tituloLibro, double monto, Date fechaGeneracion, String estado) {
+        this.id = id;
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.idPrestamo = idPrestamo;
+        this.tituloLibro = tituloLibro;
+        this.monto = monto;
+        this.fechaGeneracion = fechaGeneracion;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public int getIdPrestamo() {
+        return idPrestamo;
+    }
+
+    public void setIdPrestamo(int idPrestamo) {
+        this.idPrestamo = idPrestamo;
+    }
+
+    public String getTituloLibro() {
+        return tituloLibro;
+    }
+
+    public void setTituloLibro(String tituloLibro) {
+        this.tituloLibro = tituloLibro;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public Date getFechaGeneracion() {
+        return fechaGeneracion;
+    }
+
+    public void setFechaGeneracion(Date fechaGeneracion) {
+        this.fechaGeneracion = fechaGeneracion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
