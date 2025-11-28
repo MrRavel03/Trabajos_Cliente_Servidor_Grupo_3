7854b711ccac1e75bbe96a4fdf48b8db9018b401
Gabriel Osorio <gosorio3906@gmail.com>
2025-11-28 10:02:10 -0600

    Gestion de libros Controller

--- Proyecto_Final_Grupo3/src/main/java/biblioteca/controller/AdminDashboardController.java
+++ Proyecto_Final_Grupo3/src/main/java/biblioteca/controller/AdminDashboardController.java
@@ -2,6 +2,7 @@ package biblioteca.controller;
 
 import biblioteca.dao.UsuarioDAO;
 import biblioteca.view.AdminDashboardView;
+import biblioteca.view.GestionLibrosView;
 import biblioteca.view.GestionPrestamosView;
 import biblioteca.view.LoginView;
 
@@ -15,9 +16,9 @@ public class AdminDashboardController {
 
         this.vista.setPrestamosListener(e -> abrirGestionPrestamos());
         this.vista.setCerrarSesionListener(e -> cerrarSesion());
+        this.vista.setLibrosListener(e -> abrirGestionLibros());
 
         // TODO
-        this.vista.setLibrosListener(e -> System.out.println("Abriendo Libros..."));
         this.vista.setReservasListener(e -> System.out.println("Abriendo Reservas..."));
         this.vista.setUsuariosListener(e -> System.out.println("Abriendo Usuarios..."));
 
@@ -31,8 +32,14 @@ public class AdminDashboardController {
     }
 
     private void cerrarSesion(){
-        vista.dispose();
 
+        vista.dispose();
         new LoginController(new LoginView(), new UsuarioDAO()).iniciar();
+    }
+
+    private void abrirGestionLibros(){
+
+        GestionLibrosView vistaLibros = new GestionLibrosView();
+        new GestionLibrosController(vistaLibros);
     }
 }
---
+++ Proyecto_Final_Grupo3/src/main/java/biblioteca/controller/GestionLibrosController.java
@@ -0,0 +1,84 @@
+package biblioteca.controller;
+
+import biblioteca.dao.LibroDAO;
+import biblioteca.model.Libro;
+import biblioteca.view.GestionLibrosView;
+
+import javax.swing.*;
+import java.util.List;
+
+public class GestionLibrosController {
+
+    private final GestionLibrosView vista;
+    private final LibroDAO LibroDAO;
+
+    public GestionLibrosController (GestionLibrosView vista){
+
+        this.vista = vista;
+        this.LibroDAO = new LibroDAO();
+
+        this.vista.setAgregarListener( e -> registrarLibro());
+        this.vista.setEditarListener(System.out::println);
+        this.vista.setEliminarListener(System.out::println);
+        this.vista.setLimpiarListener(e -> vista.limpiarFormulario());
+
+        cargarTabla();
+
+        this.vista.setVisible(true);
+    }
+
+    private void cargarTabla(){
+
+        vista.getModeloTabla().setRowCount(0);
+
+        List<Libro> libros = LibroDAO.listarLibros();
+
+        for(Libro l : libros){
+            Object[] fila = {
+                    l.getId(),
+                    l.getTitulo(),
+                    l.getAutor(),
+                    l.getCategoria(),
+                    l.isDisponible() ? "Si" : "No",
+                    l.getEstado()
+            };
+            vista.getModeloTabla().addRow(fila);
+        }
+    }
+
+    private void registrarLibro(){
+
+        String titulo = vista.getTitulo();
+        String autor = vista.getAutor();
+        String categoria = vista.getCategoria();
+
+        if (titulo.isEmpty() || autor.isEmpty() || categoria.isEmpty()) {
+            JOptionPane.showMessageDialog(vista,
+                    "Por favor complete todos los campos (Título, Autor, Categoría).",
+                    "Campos vacíos",
+                    JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+
+        Libro nl = new Libro();
+
+        nl.setTitulo(titulo);
+        nl.setAutor(autor);
+        nl.setCategoria(categoria);
+
+        if (LibroDAO.registrarLibro(nl)) {
+
+            JOptionPane.showMessageDialog(vista, "Libro registrado exitosamente.");
+            vista.limpiarFormulario();
+            cargarTabla();
+
+        } else {
+            JOptionPane.showMessageDialog(
+                    vista,
+                    "Error al guardar el libro en la base de datos",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+    }
+}
